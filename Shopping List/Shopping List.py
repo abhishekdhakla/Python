ece95d4c89ca7b3d604e36ce5461061103115919
Abhishek Dhakla <abhishekdhakla@gmail.com>
2017-05-02 18:48:02 -0400

    Update Shopping List.py

--- Shopping List/Shopping List.py
+++ Shopping List/Shopping List.py
@@ -1,12 +1,30 @@
-print("Hello, I am your personal Shoping list!")
-print("Press 'DONE' to exit and see your list")
+def show_help():
+  print("Hello, I am your personal Shoping list!")
+  print("""
+Type 'DONE' to exit and see your list
+Type 'SHOW' to see the current items on the list
+Type 'HELP' to see this menu again
+""")
 
 #Making the list
 shoping_list = []
 
+show_help()
+
 #Adding items to list
 while True:
   item = input('> ')
+  
+  #Adding some functionality - HELP
+  if item == 'HELP':
+    show_help()
+    continue    
+    
+  #Adding the Show Function
+  if item == 'SHOW':
+    for temp in shoping_list:
+      print(temp)
+      continue
   
   #checking for compltetion
   if item == 'DONE':
